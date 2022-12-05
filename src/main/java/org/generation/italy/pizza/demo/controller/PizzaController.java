0da4910217faf57b9544d792a12f49f51ce165a3
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-12-05 15:09:34 +0100

    create a Pizza pojo

--- src/main/java/org/generation/italy/pizza/demo/controller/PizzaController.java
+++ src/main/java/org/generation/italy/pizza/demo/controller/PizzaController.java
@@ -1,5 +1,27 @@
 package org.generation.italy.pizza.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PizzaController {
 
+	//GET PATH without parameters
+	
+		//Indichiamo a quale path fa riferimento questo metodo
+		@RequestMapping("/")
+		//indichiamo il metodo publico che ritornerà una Stringa
+							//indichiamo la presenza di un model per fornire attributi alla view ritornata
+		public String getHome(Model model) {
+			//assegnamo alla variabile text un valore 
+			String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+						+ "	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+						+ "	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+			//aggiungiamo il valore della variabile al posto dell'elemento text inserito nell'html
+			model.addAttribute("text", text);
+			
+			//ritorniamo il file assocciato al nome "home"
+			return "index";
+		}
 }
--- src/main/java/org/generation/italy/pizza/demo/pojo/Pizza.java
+++ src/main/java/org/generation/italy/pizza/demo/pojo/Pizza.java
@@ -1,5 +1,88 @@
 package org.generation.italy.pizza.demo.pojo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+
+@Entity
+@Table
 public class Pizza {
 
+	//Indichiamo le colonne presenti nella tabella ( variabili d'istanza )
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	@NotEmpty(message = "il titolo deve contenere qualcosa")
+	@Column(length = 50)
+	private String name;
+	
+	@NotNull
+	@NotEmpty(message = "la descrizione deve contenere qualcosa")
+	@Column
+	@Lob
+	private String description;
+	
+	@NotNull(message="il prezzo non può essere nullo")
+	@Column
+	@Min(value=0)
+	private int price;
+	
+	// indichiamo il costruttore di default
+	public Pizza() {};
+	
+	//creiamo il costruttore 
+	public Pizza(String name, String description, int price) {
+
+		setName(name);
+		setDescription(description);
+		setPrice(price);
+	}
+
+	//get & set
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	};
+	
+	//toString
+	@Override
+	public String toString() {
+
+		return "Pizza : " + getId() + "-" + getName() + "-" + getDescription() + "-" + getPrice();
+	}
+	
 }
