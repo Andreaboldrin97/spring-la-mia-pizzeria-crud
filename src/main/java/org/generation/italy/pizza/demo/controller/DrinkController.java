1b262f78d89c289db17ab6892df23f2724810615
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-12-06 14:02:23 +0100

    create a drink UPDATE

--- src/main/java/org/generation/italy/pizza/demo/controller/DrinkController.java
+++ src/main/java/org/generation/italy/pizza/demo/controller/DrinkController.java
@@ -1,6 +1,7 @@
 package org.generation.italy.pizza.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.generation.italy.pizza.demo.pojo.Drink;
 import org.generation.italy.pizza.demo.service.DrinkService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.validation.Valid;
@@ -51,5 +53,18 @@ public class DrinkController {
 		
 		//a quale view ritorna
 		return "redirect:/drink";
+	}
+	
+	//metodo per modificare un record
+	@GetMapping("/drink/edit/{id}")
+	public String editDrink(@PathVariable("id") int id, Model model) {
+		
+		// selezioniamo il record con quell'id
+		Optional<Drink> optDrink= drinkService.findDrinkById(id);
+		Drink drink = optDrink.get();
+		//portiamo il record alla pagina in modo da indicare le collone che lo compongono
+		model.addAttribute("drink", drink);
+		
+		return "drinkCRUD/update";
 	}
 }
